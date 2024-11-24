de0e375397f46d2417a14ec826367dacfe68d007
Jay Kumar Gupta <100681165+jayk-gupta@users.noreply.github.com>
2024-11-24 22:48:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'https://taskify-frontend-1.onrender.com/',
+  origin: 'https://taskify-frontend-1.onrender.com',
   credentials: true
 }));
 
